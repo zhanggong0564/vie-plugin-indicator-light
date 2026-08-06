ea015dfc2cc2f2a33537a542f2ff458af1a0f434
zhanggong <zhanggong1@@sungrowpower.com>
2026-08-06 11:55:37 +0800

    fix(config): 使用指示灯v1.1检测模型

--- vie_plugin_indicator_light/config.py
+++ vie_plugin_indicator_light/config.py
@@ -13,7 +13,7 @@ class IndicatorLightConfig(SceneSettings):
         extra="ignore",
     )
 
-    det_model_path: str = "./weights/indicator_light/rfdetr-small.onnx"
+    det_model_path: str = "./weights/indicator_light/rfdetr-small_v1.1.onnx"
     rec_model_path: str = "./weights/indicator_light/rec_v3.onnx"
     det_conf_threshold: float = Field(default=0.8, ge=0, le=1)
     sim_threshold: float = Field(default=0.65, ge=0, le=1)
